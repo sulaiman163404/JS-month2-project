08cb98c44970366b38e6f04d086f524267ce7c46
unknown <sulaiman.kadyrov@gmail.com>
2023-06-09 11:42:57 +0600

    dz1

---
+++ js/home_works.js
@@ -0,0 +1,25 @@
+const emailCheck = prompt('Введите ваш email ...   (пример - xxxxxxxxxx@gmail.com')
+const regExp = /^[a-zA-Z0-9._%+-]+@gmail\.com$/ig
+
+if (emailCheck.match(regExp)){
+    console.log('Вы авторизованы')
+}else {
+    console.log('Не правильно введен email')
+}
+
+
+
+
+function moveBlock(position) {
+    const smallBlock = document.querySelector("#smallBlock");
+    smallBlock.style.left = position + "px";
+
+
+    if (position < 450) {
+        setTimeout(function() {
+            moveBlock(position + 10);
+        },120);
+    }
+}
+
+moveBlock(0);
